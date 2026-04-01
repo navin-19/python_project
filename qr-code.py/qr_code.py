69e94501e6e9cef8f1c59baa237b7dc4bf4ccf1c
Naveenkumar Boominathan <naveen.boomi019@gmail.com>
2026-04-01 14:13:25 +0530

    fixed version

--- qr-code.py/qr_code.py
+++ qr-code.py/qr_code.py
@@ -1,4 +1,12 @@
-import qrcode
-img=qrcode.make("https://youtu.be/Wq-S2FQAh_w?si=w6cee4E7WG7z0yBz")
-img.save("fav_song.jpg")
-print("created")
+try:
+    import qrcode
+except ModuleNotFoundError:
+    raise SystemExit("Install the dependency first: pip install qrcode[pil]")
+
+URL = "https://youtu.be/Kwa59h7HQzM?si=9welUZNdwkiEF0L9"
+OUTPUT_FILE = "song.png"
+
+qr_image = qrcode.make(URL)
+qr_image.save(OUTPUT_FILE)
+
+print(f"Created {OUTPUT_FILE}")
